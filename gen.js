b972955561e9a50fd729507aa356c15f74d431d3
Bhavyajot Malhotra <64335341+malhotrabhavyajot@users.noreply.github.com>
2020-07-22 14:42:24 +0530

    Update gen.js

--- gen.js
+++ gen.js
@@ -1,5 +1,4 @@
- <script>                 
- function myFunction(id) {
+function myFunction(id) {
  var str1 = document.getElementById("input1").value;
  var str2 = document.getElementById("input2").value;
  var num1 = document.getElementById("inputnum1").value;
@@ -10,4 +9,3 @@
  result = result.slice(0,5);
  document.getElementById("output1").value = result;
  }
-</script>
