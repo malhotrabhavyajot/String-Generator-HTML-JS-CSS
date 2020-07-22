e0dff57e2da3e357126b030caf5c18e1b5039fd4
Bhavyajot Malhotra <64335341+malhotrabhavyajot@users.noreply.github.com>
2020-07-22 14:10:16 +0530

    Create gen.js

---
+++ gen.js
@@ -0,0 +1,13 @@
+ <script>                 
+ function myFunction(id) {
+ var str1 = document.getElementById("input1").value;
+ var str2 = document.getElementById("input2").value;
+ var num1 = document.getElementById("inputnum1").value;
+ var num2 = document.getElementById("inputnum2").value; 
+ str1 = str1.slice(0,num1);
+ str2 = str2.slice(0,num2);
+ var result = str1.concat(str2);
+ result = result.slice(0,5);
+ document.getElementById("output1").value = result;
+ }
+</script>
